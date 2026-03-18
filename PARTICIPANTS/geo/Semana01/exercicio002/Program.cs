0a8b79e776958a3c47153dfa8794860ebc1cc2b8
Geo <geo17727@hotmail.com>
2026-03-18 13:13:11 -0300

    task 18-03

---
+++ PARTICIPANTS/geo/Semana01/exercicio002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Soma de dois números");
+
+Console.WriteLine("Digite um número: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite outro número: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite mais um número: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(num1 + num2 + num3);
+
+// comando para rodar dotnet run --project PARTICIPANTS/geo/Semana01/exercicio002dotnet run --project PARTICIPANTS/geo/exercicio003
---
+++ PARTICIPANTS/geo/Semana01/exercicio003/Program.cs
@@ -0,0 +1,7 @@
+﻿static void Saudacao(string name)
+{
+    Console.WriteLine($"Olá colega {name}, como você está?");
+}
+
+Saudacao("Geo");
+Saudacao("Andrea");
