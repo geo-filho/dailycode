e4a81ac3a9e638ae455d8034b7e046b88be64ba2
José <josehumberto3b@gmail.com>
2026-03-18 17:40:22 -0300

    task 18-03

---
+++ PARTICIPANTS/beto/exercicio001.py
@@ -0,0 +1,16 @@
+# Ex 01
+print("Beto")
+
+# Ex 02
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+
+soma = num1 + num2 + num3
+print("A soma é:", soma)
+
+# Ex 03
+def saudacao(nome):
+    return "Olá, " + nome + "! Seja bem-vindo!"
+
+print(saudacao("Beto"))
