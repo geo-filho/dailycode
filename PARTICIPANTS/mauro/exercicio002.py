0a6ef61c5577e92ed002f9f0ca7ef718545daa70
Maurogilberto <maurogilberto105@gmail.com>
2026-03-18 22:33:34 -0300

    att

---
+++ PARTICIPANTS/mauro/exercicio002.py
@@ -0,0 +1,7 @@
+num1 = float(input("Digite um numero"))
+num2 = float(input("Digite um numero"))
+num3 = float(input("Digite um numero"))
+
+soma = num1 + num2 + num3
+
+print(f"\n A soma dos números é :{soma}")
