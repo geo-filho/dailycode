3f0c750c95546e3116ab1047872cd072aa0ed7d3
Geo <geo17727@hotmail.com>
2026-03-24 14:58:34 -0300

    desafiosemanal01

---
+++ PARTICIPANTS/geo/Semana01/SEMANAL/desafio01/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Main()
+    {
+        bool autenticado = false; //declaração fora da função para garantir escopo
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("Login: ");
+            string? login = Console.ReadLine();
+
+            Console.WriteLine("Senha: ");
+            string? senha = Console.ReadLine();
+
+            autenticado = loginAuthentication(login, senha);
+
+            if (autenticado == true)
+            {
+                break;
+            }
+        }
+
+        if (!autenticado)
+        {
+            Console.WriteLine("Número de tentativas excedido.");
+        }
+    
+    }
+
+    static bool loginAuthentication(string login, string senha)
+    {
+        if(login == "admin" && senha == "123")
+        {
+            Console.WriteLine("Logado com sucesso!");
+            return true;
+        } else
+        {
+            Console.WriteLine("Insira dados válidos");
+            return false;
+        }
+    }
+}
